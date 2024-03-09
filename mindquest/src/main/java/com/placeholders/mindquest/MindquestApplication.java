acdbe1865c218b20c7d019b9fc95ffb101bbc083
Marius Ambrazevičius <mariukas.ambrazevicius@gmail.com>
2024-03-09 19:09:43 +0200

    Added base project folder

---
+++ mindquest/src/main/java/com/placeholders/mindquest/MindquestApplication.java
@@ -0,0 +1,13 @@
+package com.placeholders.mindquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindquestApplication.class, args);
+	}
+
+}
