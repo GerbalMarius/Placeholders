d6b4c2e92179b7ff85f12a9fbff836eed984b4b6
Marius Ambrazevičius <mariukas.ambrazevicius@gmail.com>
2024-03-09 19:28:13 +0200

    Added configurations in application.properties and an automatic app opener.

--- mindquest/src/main/java/com/placeholders/mindquest/MindquestApplication.java
+++ mindquest/src/main/java/com/placeholders/mindquest/MindquestApplication.java
@@ -1,13 +1,41 @@
 package com.placeholders.mindquest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class MindquestApplication {
 
+	private static final String URL = "http://localhost:8081";
+	private static final Logger logger = LoggerFactory.getLogger(MindquestApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MindquestApplication.class, args);
+		openApp();
+	}
+
+	//automatically opens browser after startup
+	private static void openApp(){
+
+		Runtime rt = Runtime.getRuntime();
+
+		String osName = System.getProperty("os.name").toLowerCase();
+
+		try {
+			//windows
+			if (osName.contains("windows")){
+				rt.exec("rundll32 url, FileProtocolHandler " + URL);
+			}else {
+				//unix
+				rt.exec("Open " + URL);
+			}
+		}catch (IOException ioxe){
+			logger.info("ERROR: COULDN'T OPEN WEBPAGE VIA URL " + URL);
+		}
 	}
 
 }
