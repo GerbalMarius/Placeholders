57b1eeb0fdfca1c9500030663212ac2773f72091
Mode03 <supermode2003@gmail.com>
2024-03-12 22:52:13 +0200

    Added starting quiz functionality

--- mindquest/src/main/java/com/placeholders/mindquest/quiz/Question.java
+++ mindquest/src/main/java/com/placeholders/mindquest/quiz/Question.java
@@ -1,2 +1,136 @@
-package com.placeholders.mindquest.quiz;public class Question {
+package com.placeholders.mindquest.quiz;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id; // Unique identifier for the question
+    private String questionTitle; // The text of the question
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private int option1points;
+    private int option2points;
+    private int option3points;
+    private int option4points;
+
+    public Question() {
+        // Default constructor
+    }
+
+    public Question(Integer id, String questionTitle, String option1, String option2, String option3, String option4, int option1points, int option2points, int option3points, int option4points) {
+        this.id = id;
+        this.questionTitle = questionTitle;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.option1points = option1points;
+        this.option2points = option2points;
+        this.option3points = option3points;
+        this.option4points = option4points;
+    }
+
+    // Getter and setter methods
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQuestionTitle() {
+        return questionTitle;
+    }
+
+    public void setQuestionTitle(String questionTitle) {
+        this.questionTitle = questionTitle;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public int getOption1points() {
+        return option1points;
+    }
+
+    public void setOption1points(int option1points) {
+        this.option1points = option1points;
+    }
+
+    public int getOption2points() {
+        return option2points;
+    }
+
+    public void setOption2points(int option2points) {
+        this.option2points = option2points;
+    }
+
+    public int getOption3points() {
+        return option3points;
+    }
+
+    public void setOption3points(int option3points) {
+        this.option3points = option3points;
+    }
+
+    public int getOption4points() {
+        return option4points;
+    }
+
+    public void setOption4points(int option4points) {
+        this.option4points = option4points;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", questionTitle='" + questionTitle + '\'' +
+                ", option1='" + option1 + '\'' +
+                ", option2='" + option2 + '\'' +
+                ", option3='" + option3 + '\'' +
+                ", option4='" + option4 + '\'' +
+                ", option1points=" + option1points +
+                ", option2points=" + option2points +
+                ", option3points=" + option3points +
+                ", option4points=" + option4points +
+                '}';
+    }
 }
---
+++ mindquest/src/main/java/com/placeholders/mindquest/startingquiz/StaringQuizController.java
@@ -0,0 +1,160 @@
+package com.placeholders.mindquest.startingquiz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Controller
+public class StaringQuizController {
+
+    @GetMapping("/startingquiz")
+    public String startingQuiz(Model model) {
+
+        model.addAttribute("quizInfo", new StartingQuizInfo());
+        return "startingQuiz";
+    }
+
+    @PostMapping("/startingquiz")
+    public String SubmitQuiz(@ModelAttribute StartingQuizInfo startingQuizInfo, Model model) {
+
+        // Calculates mental state and prints info that is submitted
+        String mentalState = calculateMentalState(startingQuizInfo);
+        System.out.println("Submitted quiz info: " + startingQuizInfo.toString());
+
+        // Writes the submitted quiz info to a text file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("quiz_info.txt", true))) {
+            writer.write(startingQuizInfo.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Returns a view with the submitted data and mental state
+        model.addAttribute("quizInfo", startingQuizInfo);
+        model.addAttribute("mentalState", mentalState);
+        return "StartingQuizResults";
+    }
+
+    private String calculateMentalState(StartingQuizInfo startingQuizInfo) {
+
+        int stressScore = calculateStressScore(startingQuizInfo.getStressLevel(), startingQuizInfo.getHowOftenFeelTired(), startingQuizInfo.getFulfillment());
+        int sleepScore = calculateSleepScore(startingQuizInfo.getRatingOfSleep(), startingQuizInfo.getHoursOfSleep(), startingQuizInfo.getHardToSleep());
+        int activityScore = calculateActivityScore(startingQuizInfo.getActivePerWeek());
+
+        int totalScore = stressScore + sleepScore + activityScore;
+        if(totalScore >= 25) {
+            return "Good";
+        } else if(totalScore >= 18) {
+            return "Fair";
+        } else {
+            return "Poor";
+        }
+    }
+
+    private int calculateStressScore(String stressLevel, String howOftenFeelTired, String fulfillment) {
+
+        int stressScore = 0;
+
+         switch (stressLevel) {
+             case "low":
+                 stressScore += 5;
+                 break;
+             case "moderate":
+                 stressScore += 3;
+                 break;
+             case "high":
+                 stressScore += 1;
+                 break;
+             default:
+                 break;
+        }
+
+        switch (howOftenFeelTired) {
+            case "rarely":
+                stressScore += 5;
+                break;
+            case "sometimes":
+                stressScore += 4;
+                break;
+            case "often":
+                stressScore += 2;
+                break;
+            case "always":
+                stressScore += 1;
+                break;
+            default:
+                break;
+        }
+
+        switch (fulfillment) {
+            case "rarely":
+                stressScore += 1;
+                break;
+            case "sometimes":
+                stressScore += 2;
+                break;
+            case "often":
+                stressScore += 4;
+                break;
+            case "always":
+                stressScore += 5;
+                break;
+            default:
+                break;
+        }
+
+        return stressScore;
+    }
+    private int calculateSleepScore(int ratingOfSleep, int hoursOfSleep, String hardToSleep) {
+
+        int sleepScore = 0;
+
+        if (ratingOfSleep >= 8) {
+            sleepScore += 5;
+        } else if (ratingOfSleep >= 6) {
+            sleepScore += 3;
+        } else if (ratingOfSleep >= 4) {
+            sleepScore += 1;
+        } else {
+            sleepScore += 0;
+        }
+
+        if (hoursOfSleep >= 7 && hoursOfSleep <= 9) {
+            sleepScore += 5; // Optimal hours of sleep
+        } else if (hoursOfSleep >= 5 && hoursOfSleep <= 11) {
+            sleepScore += 2;
+        } else {
+            sleepScore += 0;
+        }
+
+        switch (hardToSleep) {
+            case "yes":
+                sleepScore += 0;
+                break;
+            case "no":
+                sleepScore += 3;
+                break;
+            default:
+                break;
+        }
+
+        return sleepScore;
+
+    }
+    private int calculateActivityScore(String activePerWeek) {
+        return switch (activePerWeek) {
+            case "5+" -> 5; // Very active
+            case "3-4" -> 4; // Moderately active
+            case "1-2" -> 3; // Somewhat active
+            case "none" -> 1; // Inactive
+            default -> 0;
+        };
+    }
+
+}
