de1834d6975e69ab9ffb487942395a08d30ea6ac
Sarunas <sarunas.siaudvytis@gmail.com>
2024-04-08 15:38:05 +0300

    Journals controller Unit tests

--- mindquest/src/main/java/com/placeholders/mindquest/Journals/JournalController.java
+++ mindquest/src/main/java/com/placeholders/mindquest/Journals/JournalController.java
@@ -1,15 +1,9 @@
 package com.placeholders.mindquest.Journals;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.placeholders.mindquest.user_utils.User;
-import com.placeholders.mindquest.user_utils.UserDTO;
-import com.placeholders.mindquest.user_utils.UserRepository;
+
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,13 +22,6 @@ public class JournalController {
         model.addAttribute("journalList", journalList);
         return "journals";
     }
-  /*  @GetMapping("/journals")
-    public String viewJournals(Model model) {
-        List<Journal> journalList = journalRepository.findAll(); // Retrieve all journal entries from the database
-        model.addAttribute("journalList", journalList); // Add the list to the model
-        return "journals"; // Return the Thymeleaf template name
-    }
-    */
   @PostMapping("/deleteJournal")
   public String deleteJournal(@RequestParam("id") int journalId) {
 
@@ -50,15 +37,6 @@ public class JournalController {
         journalRepository.save(newJournal);
         return "redirect:/journals";
   }
- /*   @PostMapping("/updateJournal")
-    public String updateJournal(@RequestParam("id") int journalId,@ModelAttribute("diaryEntry") String content) {
-        Journal journal = journalRepository.findById(journalId).orElse(null);
-        if (journal != null) {
-            journal.setDiaryEntry(content);
-            journalRepository.save(journal);
-        }
-        return "redirect:/journals";
-    }*/
  @PostMapping("/updateJournal")
  public String updateJournal(@RequestParam("id") int journalId, @RequestParam("content") String content) {
      Journal journal = journalRepository.findById(journalId).orElse(null);
@@ -68,5 +46,8 @@ public class JournalController {
      }
      return "redirect:/journals";
  }
-
+    public void setJournalRepository(JournalRepository journalRepository) {
+        this.journalRepository = journalRepository;
+    }
 }
+
